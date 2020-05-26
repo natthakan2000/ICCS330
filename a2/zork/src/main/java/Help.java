4da513bccf3f21c61ce39a86827e984b68304b7d
Natthakan Euaumpon <natthakaneuaumpon@gmail.com>
2020-05-26 22:15:49 +0700

    help function done?

--- a2/zork/src/main/java/Help.java
+++ a2/zork/src/main/java/Help.java
@@ -1,7 +1,7 @@
 public class Help {
     public static void helper(){
         System.out.println("Available commands are: ");
-        String[] str = new String[]{"info", "take", "drop", "attack with", "go {direction}", "map", "autopilot {file}", "help", "quit", "play {map-name}, load {saved-point-name}, save {saved-point-name}", "exit"};
+        String[] str = new String[]{"info", "take", "drop", "attack with", "go {direction}", "map", "autopilot {file}", "help", "quit", "play {map-name}", "load {saved-point-name}", "save {saved-point-name}", "exit"};
         for (int i = 0; i < str.length; i++) {
             System.out.println(str[i]);
         }
