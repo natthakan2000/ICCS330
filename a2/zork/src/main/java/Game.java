0185cb7035f1f984d350052462ea2b63ee03b170
Natthakan Euaumpon <natthakaneuaumpon@gmail.com>
2020-05-31 23:44:26 +0700

    monster

--- a2/zork/src/main/java/Game.java
+++ a2/zork/src/main/java/Game.java
@@ -1,11 +1,33 @@
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
+import org.apache.commons.rng.UniformRandomProvider;
+import org.apache.commons.rng.simple.RandomSource;
 
 public class Game {
-    public static void getMonster(Boolean newGame, File rootDir) throws IOException {
+    private static final UniformRandomProvider rng = RandomSource.create(RandomSource.MT);
+    public static char[][] getMonster(Boolean newGame, File rootDir) throws IOException {
         if (newGame){
             char[][] m = Map.getNewMap(rootDir);
+            for (int i = 0; i < m.length; i++) {
+                int rand = random(m[i].length);
+                int j = 1;
+                while(j == 1){
+                    if (m[i][rand] != '|'){
+                        m[i][rand] = 'm';
+                        j = 0;
+                    }else{
+                        rand = random(m[i].length);
+                    }
+                }
+            }
+            return m;
         }
+        return null;
+    }
+    private static int random(int size){
+        return rng.nextInt(size);
+    }
+    public static void setStat(Boolean newGame){
+
     }
 }
